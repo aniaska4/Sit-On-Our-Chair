2cb1fdd1b40426f972e752b8664ea4649f19b7c7
aniaska4 <aniaska4@wp.pl>
2018-10-07 12:49:03 +0200

    poprawiony slider

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-
+//slaider
     var slider = document.querySelector(".slider"); // pobieramy slider
     var slides = document.querySelectorAll(".slider-slide"); //pobieramy zdjęcia
     var current = 0; // licznik slajdów
     var btnLeft = slider.querySelector(".btn-left"); //przycisk Lewy
     var btnRight = slider.querySelector(".btn-right");//przycisk prawy
     
-    /*przycisk LEFT*/
-    btnLeft.addEventListener('click', function(e){ 
+    /*przycisk RIGHT*/
+    btnRight.addEventListener('click', function(e){ 
         for(var el of slides){/*robię petle po zdjęciach*/
-            this.classList.remove("active"); /*usuwam zdjęciom klase active*/
+            el.classList.remove("active"); /*usuwam zdjęciom klase active*/
         }
       
 
@@ -24,19 +24,62 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
      
-    /*przycisk RIGHT*/
-    btnRight.addEventListener("click", function(e){
+    /*przycisk LEFT*/
+    btnLeft.addEventListener("click", function(e){
         for(var el of slides){
-            this.classList.remove("active");
+            el.classList.remove("active");
         }
 
         current++; /*inkrementacja bo licznikiem idziemy do góry*/
-        if(current > slides.length + 1){
+        if(current >= slides.length + 1){
             current = 0;
         }
 
         slides[current].classList.add("active");
-    })
+    });
+
+
+
+    //DROPMENU-menu
+
+    var drop = document.querySelector(".nav_item"); //li
+    var list = document.querySelector(".droopdown"); //ul
+    
+        drop.addEventListener("mouseover", function(){
+            list.style.display = "block" 
+        })
+
+        drop.addEventListener("mouseout", function(){
+            list.style.display = "none"
+        })
+
+
+    //Obrazy z oferty
+    
+    var box1 = document.querySelector(".img1"); //rodzic
+    var box2 = document.querySelector(".img2");    
+    var image = document.querySelector(".title1"); //dziecko
+    var image2 = document.querySelector(".title2");
+    
+        box1.addEventListener("mouseover", function (){
+            image.style.display = "none";
+        });
+        box1.addEventListener("mouseout", function (){
+            image.style.display = "block";
+        });
+    
+
+    
+        box2.addEventListener("mouseover", function (){
+            image2.style.display = "none";
+        });
+        box2.addEventListener("mouseout", function (){
+            image2.style.display = "block";
+        });
+    
+       
+
+
 
 
 });
